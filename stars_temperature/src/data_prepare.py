f1beb603b5bcdc9529391731e4cd523bfa34e5db
BorisovaTA <120647305+BorisovaTA@users.noreply.github.com>
2026-01-18 11:20:01 +0300

    add main files

---
+++ stars_temperature/src/data_prepare.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+def data_info(data, detailed=True):
+    '''
+    Функция для вывода описательных статистик и первичной общей информации о датасете.
+    -------
+    Параметры:
+    detailed=True - выводит доп.информацию о кол-ве пропущенных значений и дубликатов при наличии.
+    '''
+    print('=' * 80)
+    print(f'ОБЗОР ДАТАСЕТА')
+    print('=' * 80)
+
+    print(f'Основная информация:')
+    print(f'Размер данных: {data.shape[0]} строк × {data.shape[1]} столбцов')
+    display(data.head(2))
+    data.info()
+    display(data.describe().T)
+
+    if detailed:
+        print('=' * 80)
+        print('Пропущенные значения:')
+        missing_cols = data.isna().sum()
+        missing_cols = missing_cols[missing_cols > 0]
+        
+        if len(missing_cols) > 0:
+            print('Столбцы с пропусками:')
+            for col, count in missing_cols.sort_values(ascending=False).items():
+                print(f'{col}: {count:,} ({count / data.shape[0] * 100:.2f}%)')
+        else:
+            print('Пропущенных значений нет.')
+
+        print('=' * 80)
+        print('Наличие дубликатов:')
+        duplicates = data.duplicated().sum()
+        if duplicates == 0:
+            print('Явных дубликатов нет')
+        else:
+            print(f'Найдено дубликатов: {duplicates:,} ({duplicates/data.shape[0]*100:.2f}%)')
+
+        print('=' * 80)
+        print('Уникальность id')
+        if data.index.is_unique:
+            print('Все значения в поле с идентификатором уникальны')
+        else:
+            print('В поле с идентификатором есть дубликаты')
+
+def snake_case(data):
+     '''
+    Функция для приведения значений к формату 'snake_case'.
+    Автоматически определяет тип данных: названия столбцов/значения в колонке 
+    и применяет необходимые преобразования.
+    '''
+     if isinstance(data, pd.Index):
+        return (data.str.replace(r"\s*\(.*?\)", "", regex=True)
+                    .str.replace(' ', '_')
+                    .str.lower())
+     
+     elif isinstance(data, pd.Series):
+        return (data.astype(str).str.lower()
+                    .str.replace(r"[\s\-]+", "_", regex=True)
+                    .str.strip("_"))
---
+++ stars_temperature/src/visualization.py
@@ -0,0 +1,61 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+def boxplots(data, col):    
+    plt.figure(figsize=(8, 4))
+    sns.boxplot(x='star_type', y=col, data=data, palette='Set1')
+    plt.title(f'Разброс значений признаков в поле {col} по типу звезды')
+    plt.grid(True)
+    plt.show();
+
+def category_graph(data, columns):
+    for column in columns:
+        values = data[~data[column].isna()].value_counts(column)
+        ax = values.plot.barh()
+
+        xlim_max = values.max() * 1.3
+        ax.set_xlim(right=xlim_max)
+
+        for i, (count, label) in enumerate(zip(values, values.index)):
+                        ax.text(count + values.max() * 0.01, i, str(count), va='center')
+
+        plt.title(f'Столбчатая диаграмма распределения в поле "{column}"')
+        plt.xlabel('Количество звезд')
+        plt.ylabel('Значение')
+        plt.show();
+
+def numeric_graph(data, num_columns):
+    for column in num_columns:
+        data[column].hist()
+        plt.title(f'Гистограмма распределения в поле "{column}"')
+        plt.xlabel('Значение')
+        plt.ylabel('Количество звезд')
+        plt.show()   
+        
+        data[column].plot(kind='box')
+        plt.title(f'Разброс значений признаков в поле "{column}"')
+        plt.grid(True)
+        plt.show();
+
+def plot_temp_bar(y_test, y_pred):
+    y_test_np = y_test.detach().numpy().flatten()
+    y_pred_np = y_pred.detach().numpy().flatten()
+
+    indices = np.arange(len(y_test_np))
+    bar_width = 0.35
+
+    plt.figure(figsize=(14, 8))
+    plt.bar(indices, y_test_np, width=bar_width, label='Факт', color='skyblue')
+    plt.bar(indices + bar_width, y_pred_np, width=bar_width, label='Прогноз', color='orange')
+    plt.xlabel('Номер звезды в тестовой выборке')
+    plt.ylabel('Температура звезды (K)')
+    plt.legend()
+    plt.tight_layout()
+    plt.show();
+
+
+
+
+
+
